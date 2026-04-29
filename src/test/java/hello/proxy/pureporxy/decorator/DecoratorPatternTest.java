07f32747db47b77ba511352b01de825ebf3da3ba
dhson <saozzang@gmail.com>
2026-04-29 23:25:26 +0900

    [#1-10]데코레이터패턴-ex3

--- src/test/java/hello/proxy/pureporxy/decorator/DecoratorPatternTest.java
+++ src/test/java/hello/proxy/pureporxy/decorator/DecoratorPatternTest.java
@@ -1,9 +1,6 @@
 package hello.proxy.pureporxy.decorator;
 
-import hello.proxy.pureporxy.decorator.code.Component;
-import hello.proxy.pureporxy.decorator.code.DecoratorPatternClient;
-import hello.proxy.pureporxy.decorator.code.MessageDecorator;
-import hello.proxy.pureporxy.decorator.code.RealComponent;
+import hello.proxy.pureporxy.decorator.code.*;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -22,6 +19,15 @@ public class DecoratorPatternTest {
         Component realComponent = new RealComponent();
         Component messageDecorator = new MessageDecorator(realComponent);
         DecoratorPatternClient client = new DecoratorPatternClient(messageDecorator);
+        client.execute();
+    }
+
+    @Test
+    void decorator2() {
+        Component realComponent = new RealComponent();
+        Component messageDecorator = new MessageDecorator(realComponent);
+        Component timeDecorator = new TimeDecorator(messageDecorator);
+        DecoratorPatternClient client = new DecoratorPatternClient(timeDecorator);
         client.execute();
     }
 
