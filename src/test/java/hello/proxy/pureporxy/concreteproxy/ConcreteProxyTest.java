735b9f676fe28dc02c3aa946d32d8ac5da8cefc7
dhson <saozzang@gmail.com>
2026-04-30 23:06:06 +0900

    [#1-14]구체클래스기반프록시-ex2

--- src/test/java/hello/proxy/pureporxy/concreteproxy/ConcreteProxyTest.java
+++ src/test/java/hello/proxy/pureporxy/concreteproxy/ConcreteProxyTest.java
@@ -2,6 +2,7 @@ package hello.proxy.pureporxy.concreteproxy;
 
 import hello.proxy.pureporxy.concreteproxy.code.ConcreteClient;
 import hello.proxy.pureporxy.concreteproxy.code.ConcreteLogic;
+import hello.proxy.pureporxy.concreteproxy.code.TimeProxy;
 import org.junit.jupiter.api.Test;
 
 public class ConcreteProxyTest {
@@ -10,6 +11,14 @@ public class ConcreteProxyTest {
     void noProxy() {
         ConcreteLogic concreteLogic = new ConcreteLogic();
         ConcreteClient client = new ConcreteClient(concreteLogic);
+        client.execute();
+    }
+
+    @Test
+    void addProxy() {
+        ConcreteLogic concreteLogic = new ConcreteLogic();
+        TimeProxy timeProxy = new TimeProxy(concreteLogic);
+        ConcreteClient client = new ConcreteClient(timeProxy);
         client.execute();
     }
 }
